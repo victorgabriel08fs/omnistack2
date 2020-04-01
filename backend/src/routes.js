0c9e1c1503aac780aad2f71d04d4ee0a5f3eae47
victorgabriel08fs <victorgabriel08fs@gmail.com>
2020-04-01 03:39:03 -0300

    Front

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -7,7 +7,7 @@ const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
 
-routes.post('/session', SessionController.create);
+routes.post('/sessions', SessionController.create);
 
 routes.post('/ongs', OngController.create);
 routes.get('/ongs', OngController.index);
